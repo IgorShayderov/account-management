938a9bd9bb509ed4b80a2f76cc30d93ee31486b4
Igor <flashmob123@gmail.com>
2024-01-06 14:04:26 +0200

    fix: remove en locale

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,11 +1,9 @@
-import en from './locales/en';
 import ru from './locales/ru';
 
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'ru',
   messages: {
-    en,
     ru,
   },
 }));
